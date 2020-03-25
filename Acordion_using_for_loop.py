1fc28503e293408c1544e31c570e9ba994578c88
Amol Harale <amol.pushdyan11@gmail.com>
2020-03-25 17:38:36 +0530

    Accordion_using_for_loop

--- Acordion_using_for_loop.py
+++ Acordion_using_for_loop.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 from time import sleep
 
@@ -8,6 +9,33 @@ print(driver.title)
 driver.maximize_window()
 driver.switch_to.frame(0)
 sleep(4)
+
+content_head=driver.find_elements_by_css_selector('#accordion > h3')
+print("length of content_Head:", len(content_head))
+sleep(3)
+action=ActionChains(driver)
+
+i=1
+for content in content_head:
+
+    action.move_to_element(content).click().perform()
+    driver.execute_script("arguments[0].scrollIntoView();",content)
+    sleep(7)
+    Head_content=driver.find_element_by_css_selector('div[aria-labelledby="ui-id-' +str(i)+ '"] > p')
+    Head_content=Head_content.text
+    sleep(3)
+
+    print("content of section: ", Head_content)
+    print("-----------------------------------")
+
+    i+=+2
+    sleep(4)
+
+
+
+
+
+
 
 
 
